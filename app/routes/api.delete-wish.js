992470968ea3d0dc68224c12fefe24b03f6ef5ff
pradeep <pradeepsen@startbitsolutions.com>
2024-12-03 16:57:05 +0530

    change the delete api

--- app/routes/api.delete-wish.js
+++ app/routes/api.delete-wish.js
@@ -2,31 +2,33 @@ import { json } from "@remix-run/node";
 import { wishDelete } from "../model/wishDelete.server";
 import { getAccessToken } from "../model/getAccessToken.server";
 
-
-
 export const action = async ({ request }) => {
   const body = await request.json();
-  const { shop, productId ,cusId} = body;
+  const { shop, productId, cusId } = body;
 
   if (!shop && !productId && !cusId) {
-    return json({ error: "Shop, Product ID and Customer  are required." }, { status: 400 });
+    return json(
+      { error: "Shop, Product ID and Customer  are required." },
+      { status: 400 },
+    );
   }
 
   const token = { shop };
   const getToken = await getAccessToken(token);
-  
- const data={shop, productId ,cusId}
- await wishDelete(data)
 
-  return json({ message: "Product deleted from wishlist successfully." },
+  const data = { shop, productId, cusId };
+  await wishDelete(data);
+
+  return json(
+    { message: "Product deleted from wishlist successfully." },
     {
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*", 
-        "x-shopify-access-token":getToken[0].accessToken,
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, x-shopify-access-token",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
+        "x-shopify-access-token": getToken[0].accessToken,
       },
-    }
+    },
   );
 };
--- app/routes/api.save-wishlist.js
+++ app/routes/api.save-wishlist.js
@@ -1,19 +1,33 @@
 import { json } from "@remix-run/node";
 import { WishCreate } from "../model/wishCreate.server";
- 
-
+import { getAccessToken } from "../model/getAccessToken.server";
 
 export const action = async ({ request }) => {
   const body = await request.json();
-  const { shop, productId ,cusId} = body;
+  const { shop, productId, cusId } = body;
+  const token = { shop };
+  const getToken = await getAccessToken(token);
 
   if (!shop && !productId && !cusId) {
-    return json({ error: "Shop, Product ID and Customer  are required." }, { status: 400 });
+    return json(
+      { error: "Shop, Product ID and Customer  are required." },
+      { status: 400 },
+    );
   }
 
-  
- const data={shop, productId ,cusId}
- await WishCreate(data)
+  const data = { shop, productId, cusId };
+  await WishCreate(data);
 
-  return json({ message: "Product added to wishlist successfully." });
+  return json(
+    { message: "Product added to wishlist successfully." },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, x-shopify-access-token",
+        "x-shopify-access-token": getToken[0].accessToken,
+      },
+    },
+  );
 };
